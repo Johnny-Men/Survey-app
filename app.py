5e7ce92fa1e0a1f6c6e896a49b68ffc3589ee50e
Johnny <john444444@outlook.com>
2023-05-01 15:49:46 -0400

    updated session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template,redirect,flash
+from flask import Flask,request,render_template,redirect,flash,session
 from flask_debugtoolbar import DebugToolbarExtension
 from random import randint,choice,sample
 from surveys import *
@@ -9,8 +9,6 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = 'glob23'
 debug = DebugToolbarExtension(app)
 
-responses = []
-
 counter = 0
 
 @app.route('/')
@@ -19,8 +17,14 @@ def render_home_page():
     instructions = satisfaction_survey.instructions
     return render_template('base.html', title=title, instructions=instructions)
 
+@app.route('/session-list', methods=['POST'])
+def create_session_list():
+    session['responses'] = []
+    return redirect('/question/0')
+
 @app.route('/question/<int:num>')
 def render_question(num):
+    responses = session['responses']
     if num != len(responses):
         flash('Invalid question', 'error')
     else:
@@ -32,7 +36,9 @@ def render_question(num):
 
 @app.route('/answer', methods=['POST'])
 def append_answer_next():
+    responses = session['responses']
     responses.append(request.form['answer'])
+    session['responses'] = responses
     if len(responses) == len(satisfaction_survey.questions):
         return redirect('/thanks')
     else:
